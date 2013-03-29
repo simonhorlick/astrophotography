811ab4a28487e868023d9c16dc41efc8a60f17d4
Simon Horlick <simonhorlick@gmail.com>
2013-03-29 19:19:04 +0000

    Add feature detection for triangle similarity
    This detects stars using the FAST algorithm. A set of triangles is then formed
    by exhaustively enumerating all distinct triples of stars. The matching
    algorithm (to come next) is based on comparing the similarity of these
    triangles.

--- triangle_similarity_matcher.cc
+++ triangle_similarity_matcher.cc
@@ -1,10 +1,42 @@
 #include "triangle_similarity_matcher.h"
+#include <opencv2/features2d/features2d.hpp>
 
+// Features are expected to be a triple of 2d coordinates, representing the
+// triangles.
 Features TriangleSimilarityMatcher::FindFeatures( const cv::Mat& exposure ) {
-    return Features(std::vector<float>());
+    std::vector<cv::KeyPoint> stars;
+
+    cv::FAST( exposure, stars, 9, true );
+
+    std::vector<float> features;
+
+    // Return every possible distinct triangle as a feature
+    const int N = stars.size();
+    for( int i=0; i<N; i++ ) {
+        cv::Point2f& a = stars[i].pt;
+        for( int j=i; j<N; j++ ) {
+            cv::Point2f& b = stars[j].pt;
+            for( int k=j; j<N; k++ ) {
+                cv::Point2f& c = stars[k].pt;
+                EncodeFeature( features, a, b, c );
+            }
+        }
+    }
+
+    return Features(features);
 }
 
 cv::Mat TriangleSimilarityMatcher::Match( const Features& a, const Features& b ) {
     return cv::Mat();
+}
+
+void TriangleSimilarityMatcher::EncodeFeature( std::vector<float>& features,
+        const cv::Point2f& a, const cv::Point2f& b, const cv::Point2f& c ) {
+    features.push_back( a.x );
+    features.push_back( a.y );
+    features.push_back( b.x );
+    features.push_back( b.y );
+    features.push_back( c.x );
+    features.push_back( c.y );
 }
 
--- triangle_similarity_matcher.h
+++ triangle_similarity_matcher.h
@@ -2,6 +2,7 @@
 #define TRIANGLE_SIMILARITY_MATCHER_H
 
 #include "matcher.h"
+#include <opencv2/core/core.hpp>
 
 class TriangleSimilarityMatcher : public Matcher {
 public:
@@ -9,6 +10,10 @@ public:
     Features FindFeatures( const cv::Mat& exposure );
 
     cv::Mat Match( const Features& a, const Features& b );
+
+    void EncodeFeature( std::vector<float>& features, const cv::Point2f& a,
+            const cv::Point2f& b, const cv::Point2f& c );
+
 };
 
 #endif // TRIANGLE_SIMILARITY_MATCHER_H
