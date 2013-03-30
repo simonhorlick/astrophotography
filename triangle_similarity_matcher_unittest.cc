fde9bc3efb1e4d9564c0ec116e707048a8d1b7c4
Simon Horlick <simonhorlick@gmail.com>
2013-03-30 20:58:15 +0000

    Add test for different vertex ordering

--- triangle_similarity_matcher_unittest.cc
+++ triangle_similarity_matcher_unittest.cc
@@ -48,3 +48,26 @@ TEST(TriangleSimilarityMatcher, Rotation) {
     FAIL() << "Not implemented";
 }
 
+TEST(TriangleSimilarityMatcher, TranslationDifferentVertexOrdering) {
+    TriangleSimilarityMatcher m;
+
+    std::vector<float> featuresA, featuresB;
+
+    // Try matching a translation of (2,2)
+    m.EncodeFeature( featuresA, cv::Point2f(0,0), cv::Point2f(1,0), cv::Point2f(0,5) );
+    m.EncodeFeature( featuresB, cv::Point2f(3,2), cv::Point2f(2,7), cv::Point2f(2,2) );
+
+    cv::Mat actual = m.Match( featuresA, featuresB );
+    float data[] = {
+        1,0,0,2,
+        0,1,0,2,
+        0,0,1,0,
+        0,0,0,1,
+    };
+    cv::Mat expected( 4,4, CV_32F, data );
+
+    // TODO: Find out how to compare cv::Mats
+    // EXPECT_EQ( actual, expected );
+    FAIL() << "Not implemented";
+}
+
